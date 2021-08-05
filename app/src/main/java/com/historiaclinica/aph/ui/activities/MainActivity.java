7ff7bc231d186acac1f3304ad6487acb6a4de812
Noack <noackjulio@gmail.com>
2021-08-05 16:05:28 -0500

    creacion de navigation

---
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/MainActivity.java
@@ -0,0 +1,6 @@
+package com.historiaclinica.aph.ui.activities;
+
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends Fragment {
+}
--- app/src/main/java/com/historiaclinica/aph/ui/fragments/InicioDetailDialogFragment.java
+++
@@ -1,6 +0,0 @@
-package com.historiaclinica.aph.ui.fragments;
-
-import android.app.Activity;
-
-public class InicioDetailDialogFragment extends Activity {
-}
