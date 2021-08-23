3ae6dd0cf24e65d7622742287795bb7749f37cd4
Noack <noackjulio@gmail.com>
2021-08-23 16:59:41 -0500

    inicio de animacion

--- app/src/main/java/com/historiaclinica/aph/ui/activities/SplashscreeActivity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/SplashscreeActivity.kt
@@ -2,11 +2,25 @@ package com.historiaclinica.aph.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import com.historiaclinica.aph.R
 
 class SplashscreeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscree)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
+        ivlogo_aph.starAnimation(desplazamiento_arriba)
+
+
+
     }
+
+
+
 }
