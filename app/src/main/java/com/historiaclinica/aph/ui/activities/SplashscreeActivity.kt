60d7d613e6ea50708ce25d161cf202b958e7b0a2
Noack <noackjulio@gmail.com>
2021-08-23 18:39:26 -0500

    rediseño del splash

--- app/src/main/java/com/historiaclinica/aph/ui/activities/SplashscreeActivity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/SplashscreeActivity.kt
@@ -3,23 +3,19 @@ package com.historiaclinica.aph.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import com.historiaclinica.aph.R
-import com.historiaclinica.aph.R.anim.desplazamiento_arriba
-
 
 class SplashscreeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscree)
-        setContentView(desplazamiento_arriba)
 
-        val animation = AnimationUtils.loadAnimation(this, desplazamiento_arriba)
-        logosplash.starAnimation(desplazamiento_arriba)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba)
+
 
         val intent = Intent(this, MainActivity::class.java)
 
