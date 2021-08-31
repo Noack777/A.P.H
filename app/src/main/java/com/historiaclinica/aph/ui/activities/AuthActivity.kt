741fee93312b0dc5fdb3a4b3cbb89aef3fa3f37d
Noack <noackjulio@gmail.com>
2021-08-31 17:56:19 -0500

    Auth Exitoso

--- app/src/main/java/com/historiaclinica/aph/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/AuthActivity.kt
@@ -1,57 +1,72 @@
 package com.historiaclinica.aph.ui.activities
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.historiaclinica.aph.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
 
-        //lanzamiento evento en Google analytics
-        val analytics = FirebaseAnalytics.getInstance(this)
-        val bundle = Bundle();
-        bundle.putString("messeage","Integracion de Firebase Completa")
-        analytics.logEvent("InitScreen" ,bundle)
+        // Analytics Event
+        val analytics:FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
 
-        //accion boton 2
-        botonadmin.setOnClickListener {
-            val intent:Intent = Intent (this, Login_Admin_Activity::class.java)
-            startActivity(intent)
-        }
+        // Setup
+        setup()
     }
 
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
+    private fun setup() {
 
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
+        //Logica del Boton de ingreso
+
+        botonlogin.setOnClickListener{
+
+            if (codigologueo.text.isNotEmpty() && Contrasenia.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(codigologueo.text.toString(),
+                        Contrasenia.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+
+                            showinicio(it.result?.user?.email ?: "", ProviderType.BASIC)
+
+                        } else {
+
+                            showAlert()
+
+                        }
+                    }
             }
-        // [END sign_in_with_email]
+
+        }
+
     }
 
+    private fun showAlert (){
 
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showinicio(email: String, provider: ProviderType) {
+
+        val inicioIntent = Intent(this, InicioActivity::class.java).apply {
+
+        }
+
+        startActivity(inicioIntent)
+
+    }
 }
--- app/src/main/java/com/historiaclinica/aph/ui/activities/InicioActivity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/InicioActivity.kt
@@ -3,10 +3,16 @@ package com.historiaclinica.aph.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.historiaclinica.aph.R
+import kotlinx.android.synthetic.main.activity_auth.*
+
+enum class ProviderType {
+    BASIC
+}
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_inicio)
+
     }
 }
