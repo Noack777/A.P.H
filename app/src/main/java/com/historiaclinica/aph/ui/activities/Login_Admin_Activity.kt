6b4d3dab57a0f31ab0f3a3d9088674d08acf704d
Brayan-02 <89172871+Brayan-02@users.noreply.github.com>
2021-09-09 18:10:54 -0500

    REGuSUA
    REGISTRO USUARIO

--- app/src/main/java/com/historiaclinica/aph/ui/activities/Login_Admin_Activity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/Login_Admin_Activity.kt
@@ -3,6 +3,8 @@ package com.historiaclinica.aph.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import com.historiaclinica.aph.R
 import kotlinx.android.synthetic.main.fragment_login__admin.*
 
@@ -18,5 +20,44 @@ class Login_Admin_Activity : AppCompatActivity() {
             finish()
 
         }
+
+        setup()
     }
+
+    private fun setup() {
+        botonloginadmin.setOnClickListener {
+            if (nombreUsua.text!!.isNotEmpty() && RContrasenia.text!!.isNotEmpty()) {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(nombreUsua.text.toString()
+                ,RContrasenia.text.toString()).addOnCompleteListener {
+
+                    if (it.isSuccessful){
+                        regbien(it.result?.user?.email ?: "",ProviderType.BASIC)
+                    }else{
+                        alerta()
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun alerta(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al crear la cuenta")
+        builder.setPositiveButton("aceptar",null)
+        val dialog : AlertDialog =builder.create()
+        dialog.show()
+
+    }
+
+    private fun regbien(email:String,provider:ProviderType){
+        val InicioActivityIntent = Intent(this,InicioActivity::class.java).apply {
+            putExtra("email",email)
+            putExtra("provaider",provider.name)
+        }
+        startActivity(InicioActivityIntent)
+    }
+
 }
+
