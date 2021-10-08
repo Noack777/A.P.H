a0a4b2d942501515e5adf8e50158b39dcf987f4e
Brayan-02 <89172871+Brayan-02@users.noreply.github.com>
2021-10-08 00:17:28 -0500

    Creacion DE artchivo Pdf

--- app/src/main/java/com/historiaclinica/aph/ui/activities/InicioActivity.kt
+++ app/src/main/java/com/historiaclinica/aph/ui/activities/InicioActivity.kt
@@ -12,13 +12,16 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -26,21 +29,60 @@ import androidx.core.content.ContextCompat
 import com.airbnb.lottie.LottieAnimationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.historiaclinica.aph.R
+import com.itextpdf.text.Document
+import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfWriter
 import kotlinx.android.synthetic.main.fragment_inicio.*
+import java.io.FileOutputStream
 import java.io.IOException
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 enum class ProviderType {
     BASIC
 }
+//variables guardar pdf
 
+
+
+
+//varibles grabar audio
 private const val LOG_TAG = "AudioRecordTest"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class InicioActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.fragment_inicio)
+
+    private lateinit var et_pdf_data : EditText
+    private lateinit var btn_generar_Pdf : EditText
+    private val STORAGE_CODE = 1001
+
+
+    private fun savePDF() {
+        val mDoc = Document()
+        val mFileName = SimpleDateFormat("yyyHHdd_HHmmss", Locale.getDefault())
+            .format(System.currentTimeMillis())
+
+        val mFilepath = Environment.getExternalStorageDirectory().toString() + "/" + mFileName + ".pdf"
+
+        try {
+
+            PdfWriter.getInstance(mDoc, FileOutputStream(mFilepath))
+            mDoc.open()
+
+            val data = et_pdf_data.text.toString().trim()
+            mDoc.addAuthor("TecnoDesing")
+            mDoc.add(Paragraph(data))
+            mDoc.close()
+            Toast.makeText(this, "$mFileName.pdf\n es creado  en \n $mFilepath", Toast.LENGTH_SHORT).show()
+
+        }catch (e:Exception){
+            Toast.makeText(this, ""+e.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 
     //variables grabar audio
     private var fileName: String = ""
@@ -52,11 +94,19 @@ class InicioActivity : AppCompatActivity() {
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
 
     //permiso para dispositivo
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
+        when(requestCode){
+            STORAGE_CODE ->{
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    savePDF()
+                }else{
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
             grantResults[0] == PackageManager.PERMISSION_GRANTED
@@ -74,6 +124,7 @@ class InicioActivity : AppCompatActivity() {
         fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+
 
         //logica boton grabar
 
@@ -100,6 +151,8 @@ class InicioActivity : AppCompatActivity() {
                 }
                 start()
            }
+
+
         }
 
         //logica boton detener grabar
@@ -146,6 +199,25 @@ class InicioActivity : AppCompatActivity() {
             cambio3 = cambio_icon(btn_detener_repro, R.raw.animation_stop, cambio3)
         }
 
+//PARTE LOGICA CREACION PDF
+        et_pdf_data = findViewById(R.id.b1_date1)
+
+
+        finalizar.setOnClickListener {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ){
+                    val permission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    requestPermissions(permission, STORAGE_CODE)
+                }else{
+                    savePDF()
+                }
+            }else{
+                savePDF()
+            }
+        }
+
     }
 
     //Logica para cambiar de icono al presionar el boton
@@ -158,7 +230,8 @@ class InicioActivity : AppCompatActivity() {
         else {
             imageView.setImageResource(R.drawable.icon_grabar)
         }
-        return !cambio }
+        return !cambio
+    }
 
     private fun cambio_icon1(imageView: LottieAnimationView, animation: Int,cambio1: Boolean) : Boolean {
         if (!cambio1) {
@@ -168,7 +241,8 @@ class InicioActivity : AppCompatActivity() {
         else {
             imageView.setImageResource(R.drawable.icon_stop)
         }
-        return !cambio1 }
+        return !cambio1
+    }
 
     private fun cambio_icon2(imageView: LottieAnimationView, animation: Int,cambio2: Boolean) : Boolean {
         if (!cambio2) {
@@ -178,8 +252,8 @@ class InicioActivity : AppCompatActivity() {
         else {
            restaurar_play(btn_reproducir)
         }
-        return !cambio2 }
-
+        return !cambio2
+    }
     private fun cambio_icon3(imageView: LottieAnimationView, animation: Int,cambio3: Boolean) : Boolean {
         if (!cambio3) {
             imageView.setAnimation(animation)
@@ -188,24 +262,24 @@ class InicioActivity : AppCompatActivity() {
         else {
             restaurar_stop1(btn_detener_repro)
         }
-        return !cambio3 }
+        return !cambio3
+    }
+
 
     //logica para mostrar mensajes al presiomar el boton
     private fun mostrarmensaje() {
         Toast.makeText(this, "Grabación Iniciada", Toast.LENGTH_LONG).show()
     }
-
     private fun mostrarmensaje1() {
         Toast.makeText(this, "Grabación Detenida", Toast.LENGTH_LONG).show()
     }
-
     private fun mostrarmensaje2() {
         Toast.makeText(this, "Reproduciendo...", Toast.LENGTH_LONG).show()
     }
-
     private fun mostrarmensaje3() {
         Toast.makeText(this, "Reproducción Finalizada", Toast.LENGTH_LONG).show()
     }
+
 
     // funcion  Restaurar
     private fun restaurar_Grabar(imageView: LottieAnimationView) {
